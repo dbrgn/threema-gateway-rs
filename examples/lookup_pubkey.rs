f710bffb395a245fd2cc429b46605b61a31d25ad
Danilo Bargen <mail@dbrgn.ch>
2016-11-25 22:24:44 +0100

    Add connection::lookup_pubkey

---
+++ examples/lookup_pubkey.rs
@@ -0,0 +1,30 @@
+extern crate docopt;
+extern crate threema_gateway;
+
+use docopt::Docopt;
+use threema_gateway::connection::lookup_pubkey;
+
+
+const USAGE: &'static str = "
+Usage: send_e2e [options] <our_id> <secret> <their_id>
+
+Options:
+    -h, --help    Show this help
+";
+
+
+fn main() {
+    let args = Docopt::new(USAGE)
+                      .and_then(|docopt| docopt.parse())
+                      .unwrap_or_else(|e| e.exit());
+
+    // Command line arguments
+    let our_id = args.get_str("<our_id>");
+    let their_id = args.get_str("<their_id>");
+    let secret = args.get_str("<secret>");
+
+    // Fetch public key
+    let pubkey = lookup_pubkey(our_id, their_id, secret);
+
+    println!("Public key for {} is {}.", their_id, pubkey);
+}
--- src/connection.rs
+++ src/connection.rs
@@ -6,6 +6,25 @@ use url::form_urlencoded;
 use data_encoding::hex;
 
 
+/// Fetch the public key for the specified Threema ID.
+///
+/// For the end-to-end encrypted mode, you need the public key of the recipient
+/// in order to encrypt a message. While it's best to obtain this directly from
+/// the recipient (extract it from the QR code), this may not be convenient,
+/// and therefore you can also look up the key associated with a given ID from
+/// the server.
+///
+/// It is strongly recommended that you cache the public keys to avoid querying
+/// the API for each message.
+pub fn lookup_pubkey(our_id: &str, their_id: &str, secret: &str) -> String {
+    let client = Client::new();
+    let url = format!("https://msgapi.threema.ch/pubkeys/{}?from={}&secret={}", their_id, our_id, secret);
+    let mut res = client.get(&url).send().unwrap();
+    let mut body = String::new();
+    res.read_to_string(&mut body).expect("Could not read response body");
+    body
+}
+
 /// Send an already encrypted E2E message to the specified receiver.
 pub fn send_e2e(from: &str, to: &str, secret: &str, nonce: &[u8], ciphertext: &[u8]) -> String {
     let client = Client::new();
