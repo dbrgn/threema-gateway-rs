5a4bfaf1898d1b486fdd46a0214cf70f1a31e494
Travis CI User <travis@example.org>
2018-04-09 16:12:57 +0000

    Documentation for dbrgn/threema-gateway-rs@6257c05

---
+++ implementors/core/fmt/trait.Pointer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["crossbeam_epoch"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"crossbeam_epoch/struct.Atomic.html\" title=\"struct crossbeam_epoch::Atomic\">Atomic</a>&lt;T&gt;","impl&lt;'g, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"crossbeam_epoch/struct.Shared.html\" title=\"struct crossbeam_epoch::Shared\">Shared</a>&lt;'g, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ threema_gateway/errors/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ApiBuilderError","Errors when interacting with the `ApiBuilder`."],["ApiError","Errors when interacting with the API."],["CryptoError","Crypto related errors."]]});
