cacbe96244e3039f5219b3e2f5ce7c540e5daa55
Danilo Bargen <danilo.bargen@threema.ch>
2026-07-10 18:32:35 +0200

    Bump sqlx: 0.8 → 0.9

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -81,7 +81,7 @@ where
 {
     fn encode_by_ref(
         &self,
-        buf: &mut <DB as sqlx::Database>::ArgumentBuffer<'enc>,
+        buf: &mut <DB as sqlx::Database>::ArgumentBuffer,
     ) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
         <Vec<u8> as sqlx::Encode<'enc, DB>>::encode(self.0.to_vec(), buf)
     }
--- src/protocol/threema_id.rs
+++ src/protocol/threema_id.rs
@@ -147,7 +147,7 @@ where
 {
     fn encode_by_ref(
         &self,
-        buf: &mut <DB as sqlx::Database>::ArgumentBuffer<'enc>,
+        buf: &mut <DB as sqlx::Database>::ArgumentBuffer,
     ) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
         <String as sqlx::Encode<'enc, DB>>::encode(self.as_str().to_owned(), buf)
     }
